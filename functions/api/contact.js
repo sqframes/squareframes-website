86fd6268ff9a9a813b7ee4ef4b0e61bb05054a9c
sqframes <pranitk01@gmail.com>
2026-01-07 09:07:10 +1200

    Update contact.js

--- functions/api/contact.js
+++ functions/api/contact.js
@@ -5,11 +5,11 @@ const mcPayload = {
   personalizations: [
     {
       to: [{ email: toAddress }],
-      cc: [{ email }], // user gets a copy
+      cc: [{ email }], // user copy (optional)
     },
   ],
   from: { email: fromAddress, name: "Square Frames" },
-  reply_to: { email, name }, // reply goes to customer
+  reply_to: { email, name },
   subject,
   content: [
     { type: "text/plain", value: textBody },
